1b4f425c02b1d4fa08cb43e0ea6aecb296e0955c
stack72 <public@paulstack.co.uk>
2015-06-01 18:49:40 +0100

    Adding a simple module that will look up the latest DIGIT Base AMI ID

---
+++ getvariables.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby
+require 'json'
+
+profiles = []
+File.open(File.expand_path('~/.aws/credentials'), 'r') do |f|
+  f.each_line do |l|
+    next unless l.gsub!(/^\[\s*(\w+)\s*\].*/, '\1')
+    l.chomp!
+    next if l == 'default'
+    profiles.push(l)
+  end
+end
+
+base_amis = Hash.new
+
+data = profiles.map do |account|
+  regions_json = `aws ec2 describe-regions --profile #{account} --region us-east-1`
+  if $?.exitstatus != 0
+    print "Failed to run aws ec2 describe-regions --profile #{account}"
+    exit 1
+  end
+  regions = JSON.parse(regions_json)['Regions'].map { |d| d['RegionName'] }
+  regions.map do |region|
+    images_json = `aws ec2 describe-images --profile #{account} --region #{region} --filters "Name=tag-key,Values=Name" "Name=tag-value,Values=base-image"`
+    if $?.exitstatus != 0
+      print "Failed to run aws ec2 describe-images --profile #{account} --region us-east-1"
+      exit 1
+    end
+    images = JSON.parse(images_json)['Images'].sort_by { |hash| hash['ImageId'].to_i }
+    unless images.empty?
+      latest_image = images.last
+      base_amis.merge!(Hash["#{region}" => latest_image["ImageId"]])
+    end
+  end
+end
+
+output = {
+  "digit_base_ami_id" => {
+    "description" => "The DIGIT base ami",
+    "default" => base_amis
+  }
+}
+
+File.open('variables.tf.json.new', 'w') { |f| f.puts JSON.pretty_generate(output) }
---
+++ main.tf
@@ -0,0 +1,5 @@
+variable "region" {}
+
+output "base_ami_id" {
+    value = "${lookup(var.digit_base_ami_id, var.region))}"
+}
